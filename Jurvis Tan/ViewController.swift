04b39dfed36ee73765ab27f0ecca3c264a0e3d14
Jurvis Tan <jurvis.tan@me.com>
2015-04-15 10:31:26 +0800

    Initial Commit

---
+++ Jurvis Tan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jurvis Tan
+//
+//  Created by Jurvis Tan on 15/4/15.
+//  Copyright (c) 2015 Jurvis Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
